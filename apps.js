cef866bc83c87470dd47362e03e60b28d62f1fe4
John Finley <jfinley6@mail.csuchico.edu>
2021-09-11 18:33:31 -0400

    Everything but the equals key is working

--- apps.js
+++ apps.js
@@ -8,23 +8,126 @@ const result = document.getElementById("operatorResult");
 const operatorButtons = document.querySelectorAll(".operatorButton");
 const equals = document.getElementById("equals");
 
-
+currentNumber.innerHTML = "0";
+let storedNumber = "";
+let firstNumber = "";
+const calcResult = "";
+let clickedOperator = "";
+let finalResult = "";
 
 //Math functions to return based on which operator the user has entered
-myFunctions = [
-	function (num1, num2) {
-		return num1 + num2;
-	},
-	function (num1, num2) {
-		return num1 - num2;
-	},
-	function (num1, num2) {
-		return num1 / num2;
-	},
-	function (num1, num2) {
-		return num1 * num2;
-	},
-];
+function add(a, b) {
+	return a + b;
+}
+
+function subtract(a, b) {
+	return a - b;
+}
+
+function multiply(a, b) {
+	return a * b;
+}
+
+function divide(a, b) {
+	return a / b;
+}
+
+function operate(num1, num2, operator) {
+	switch (operator) {
+		case "+":
+			return add(num1, num2);
+		case "-":
+			return subtract(num1, num2);
+		case "x":
+			return multiply(num1, num2);
+		case "÷":
+			return divide(num1, num2);
+	}
+}
+
+// Shows the current number that the user has entered
+numbers.forEach((number) => {
+	number.addEventListener("click", function () {
+		if (currentNumber.innerHTML.includes(".") && this.innerHTML === ".") {
+			return;
+		}
+		if (currentNumber.innerHTML === "0") {
+			currentNumber.innerHTML = "";
+		}
+		if (clickedOperator === "") {
+			storedNumber += this.innerHTML;
+			currentNumber.innerHTML = storedNumber;
+		} else if (clickedOperator && !firstNumber) {
+			firstNumber = result.innerHTML.slice(0, -2);
+			storedNumber = this.innerHTML;
+			currentNumber.innerHTML = storedNumber;
+		} else {
+			storedNumber += this.innerHTML;
+			currentNumber.innerHTML = storedNumber;
+		}
+	});
+});
+
+operatorButtons.forEach((operator) => {
+	operator.addEventListener("click", function () {
+		if (clickedOperator === "÷" && storedNumber === "0") {
+			result.innerHTML = "Nan";
+			currentNumber.innerHTML = "Don't Divide By 0";
+			setTimeout(resetScreen, 2000);
+		} else if (clickedOperator && storedNumber) {
+			calculate();
+			clickedOperator = this.innerHTML;
+			result.innerHTML = finalResult + " " + clickedOperator;
+			storedNumber = "";
+			currentNumber.innerHTML = "";
+			firstNumber = result.innerHTML.slice(0, -2);
+		} else if (clickedOperator && !storedNumber) {
+			clickedOperator = this.innerHTML;
+			result.innerHTML = firstNumber + " " + clickedOperator;
+		} else {
+			firstNumber = storedNumber;
+			storedNumber = "";
+			clickedOperator = this.innerHTML;
+			result.innerHTML = firstNumber + " " + clickedOperator;
+			currentNumber.innerHTML = "";
+		}
+	});
+});
+
+function calculate() {
+	const calcResult = operate(parseFloat(firstNumber), parseFloat(storedNumber), clickedOperator);
+	if (clickedOperator === "÷" && storedNumber === "0") {
+		result.innerHTML = calcResult;
+		finalResult = calcResult;
+	} else {
+		result.innerHTML = calcResult + " " + clickedOperator;
+		finalResult = calcResult;
+	}
+}
+
+equals.addEventListener("click", function () {
+	if (clickedOperator === "÷" && storedNumber === "0") {
+		result.innerHTML = "Nan";
+		currentNumber.innerHTML = "Don't Divide By 0";
+		setTimeout(resetScreen, 2000);
+	}
+});
+
+resetButton.addEventListener("click", resetScreen());
+
+deleteButton.addEventListener("click", function () {
+	storedNumber = storedNumber.slice(0, -1);
+	currentNumber.innerHTML = storedNumber;
+});
+
+function resetScreen() {
+	storedNumber = "";
+	finalResult = "";
+	firstNumber = "";
+	clickedOperator = "";
+	result.innerHTML = "";
+	currentNumber.innerHTML = "0";
+}
 
 
 
